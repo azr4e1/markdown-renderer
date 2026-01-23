377fe5b670599afdcf3b2320853a11221c7158a4
Lorenzo Drumond <drumondlorenzo@posteo.net>
2026-01-23 01:03:02 +0000

    chore: commit save point

--- block_parser.go
+++ block_parser.go
@@ -6,6 +6,9 @@ import (
 
 const (
 	BLOCKDELIMITER = "\n\n"
+	BREAKDELIMITER = "---"
+	CODEDELIMITER  = "```"
+	QUOTEDELIMITER = ">"
 )
 
 func MarkdownToBlocks(markdown string) []string {
@@ -21,3 +24,36 @@ func MarkdownToBlocks(markdown string) []string {
 
 	return cleanBlocks
 }
+
+func BlockParser(block string) Block {
+	switch {
+	case isBreak(block):
+		return Break(true)
+	case isCode(block):
+		return codeify(block)
+	case isQuote(block):
+		return quoteify(block)
+	case isOrderedList(block):
+		return listify(block, 0)
+	case isUnorderedList(block):
+		return listify(block, 1)
+	case isTable(block):
+		return tableify(block)
+	default:
+		return Paragraph{Text(block)}
+	}
+}
+
+func isBreak(block string) bool {
+	return block == BREAKDELIMITER
+}
+
+func isCode(block string) bool {
+	return strings.HasPrefix(block, CODEDELIMITER) && strings.HasSuffix(block, CODEDELIMITER)
+}
+
+func codeify(block string) Code {
+	return Code(strings.Trim(block, CODEDELIMITER))
+}
+
+func isQuote
--- core.go
+++ core.go
@@ -4,20 +4,8 @@ type Text string
 type Bold Text
 type Italic Text
 type Underline Text
+type InlineCode Text
 type Crossed Text
-
-type Header struct {
-	Content []Text
-	Level   int
-}
-
-type Code Text
-type Quote []Text
-
-type ListItem []Text
-type UnorderedItem ListItem
-type OrderedItem ListItem
-type List []ListItem
 
 type Hyperlink struct {
 	Content Text
@@ -28,6 +16,21 @@ type Image struct {
 	Content Text
 	Path    string
 }
+type Header struct {
+	Content []Text
+	Level   int
+}
+
+type Paragraph []Text
+type Code Text
+type Quote []Text
+type Break bool
+
+type ListItem []Text
+type UnorderedItem ListItem
+type OrderedItem ListItem
+type OrderedList []OrderedItem
+type UnorderedList []UnorderedItem
 
 type TableItem []Text
 type TableHeader []TableItem
@@ -35,4 +38,30 @@ type TableRow []TableItem
 type Table struct {
 	Header TableHeader
 	Rows   []TableRow
+}
+
+type Block interface {
+	isBlock() bool
+}
+
+func (b Paragraph) isBlock() bool {
+	return true
+}
+func (b Code) isBlock() bool {
+	return true
+}
+func (b Quote) isBlock() bool {
+	return true
+}
+func (b Break) isBlock() bool {
+	return true
+}
+func (b OrderedList) isBlock() bool {
+	return true
+}
+func (b UnorderedList) isBlock() bool {
+	return true
+}
+func (b Table) isBlock() bool {
+	return true
 }
